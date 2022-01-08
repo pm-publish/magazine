743746e9c1ee454f498725e88584cab5f432293b
inspheremayank <insphere.mayank@gmail.com>
2022-01-09 02:01:11 +0530

    added homepage

--- static/js/vendor.js
+++ static/js/vendor.js
@@ -59,5 +59,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
             "",
             ""
         ]
-    });   
+    });
+    
+    $('.home_slider').owlCarousel({
+        margin:30,
+        responsiveClass:true,
+        navText: [
+            "",
+            ""
+        ],
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
 });
