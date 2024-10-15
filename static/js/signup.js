be78232f63d6188610debbba3c56d82a8c398bf4
inspheremayank <insphere.mayank@gmail.com>
2024-10-15 10:17:28 +0530

    error message fixes

--- static/js/signup.js
+++ static/js/signup.js
@@ -205,7 +205,7 @@ SubscribeForm.prototype.submit = async function(event)
                 self.signupModal.closeWindow();
                 // Inform the user if there was an error
                 var errorElement = document.getElementById('card-errors');
-                errorElement.textContent = result.error;
+                errorElement.textContent = result.error.message;
             } else {
                 // Send the token to your server
 
